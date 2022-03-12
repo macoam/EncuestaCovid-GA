cb71db30770ba11bb761d38b4a2e5b45c51a12ad
Marianne <53828111+macoam@users.noreply.github.com>
2022-03-11 17:52:33 -0700

    Add files via upload

---
+++ CovidEncuesta/CovidEncuenta/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  CovidEncuenta
+//
+//  Created by Alumno on 3/11/22.
+//  Copyright © 2022 Alumno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var btnAlumno: UIButton!
+    @IBOutlet weak var btnMaestro: UIButton!
+    @IBOutlet weak var btnEmpleado: UIButton!
+    
+    @IBAction func Alumno(_ sender: UIButton) {
+    }
+    
+    @IBAction func Maestro(_ sender: UIButton) {
+    }
+    
+    @IBAction func Empleado(_ sender: UIButton) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
